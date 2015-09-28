233c3b7d3b888f10f6d2fed2f2f47ea6339cb639
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 12:15:57 -0700

    basic counter working

--- wordcount.py
+++ wordcount.py
@@ -5,9 +5,13 @@ for line in text_file:
     words = line.split(" ")
     for word in words:
         word = word.rstrip()
+
+        #if word not found, add it
         if word not in word_counts:
             word_counts[word] = 1
-        else:  # word already in word_counts
+
+        # otherwise, word already in word_counts, so increment count
+        else:  
             word_counts[word] += 1
 
 for word, count in word_counts.items():
