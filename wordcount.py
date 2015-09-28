48695dd1afcdb868e99457b55255d2694c830f88
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 12:26:42 -0700

    lowercasing and stripping punctuation working

--- wordcount.py
+++ wordcount.py
@@ -4,7 +4,9 @@ word_counts = {}
 for line in text_file:
     words = line.split(" ")
     for word in words:
-        word = word.rstrip()
+
+        #strip all punctuation and lowercase word
+        word = word.strip("\'\"\n\t-,./?!:;()*[]").lower()
 
         # #if word not found, add it
         # if word not in word_counts:
@@ -17,7 +19,7 @@ for line in text_file:
         #if word not found, add it
         if word_counts.get(word) == None:
             word_counts[word] = 1
-            
+
         # otherwise, word already in word_counts, so increment count
         else:
             word_counts[word] += 1
